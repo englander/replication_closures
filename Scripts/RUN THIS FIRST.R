29f48f7cb5866c2983eef5543868f36f26229e2b
englander <aenglander@worldbank.org>
2022-12-07 11:02:54 -0500

    install devtools and renv 0.15.5 at start of script

--- Scripts/RUN THIS FIRST.R
+++ Scripts/RUN THIS FIRST.R
@@ -1,7 +1,13 @@
 #This script ensures you are using the same package versions that I did
 
+install.packages('devtools')
+
+devtools::install_version("renv", version = "0.15.5", repos = "http://cran.us.r-project.org")
+
 #If using Windows OS, set download method
 if(Sys.info()["sysname"] == "Windows"){
   Sys.setenv(RENV_DOWNLOAD_METHOD = "wininet")
 }
+
 renv::restore()
+#If R asks whether you want to "activate", enter Y into the console
